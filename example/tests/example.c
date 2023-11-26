c54b795c211f3ed6323360150cde68ab81c5215e
Jordy Aaldering <jordy.aaldering@ru.nl>
2023-11-26 17:37:00 +0100

    multiline strings

--- example/tests/example.c
+++ example/tests/example.c
@@ -5,18 +5,18 @@ FEATURE(example, feature1, feature2)
 
 SCENARIO(feature1)
     GIVEN(a_sac_program,
-        "not a sac program\n")
+        "not a sac program")
     WHEN(i_compile_it_with, "-noprelude")
     THEN(compilation_failed)
     THEN(sac2c_output_contains, "Failed to construct a syntax tree", 1)
 ENDSCENARIO
 
 SCENARIO(feature2)
-    GIVEN(a_sac_program,
-        "int main()\n"
-        "{\n"
-        "    return 0;\n"
-        "}\n")
+    GIVEN(a_sac_program, STR(
+        int main()
+        {
+            return 0;
+        }))
     WHEN(i_compile_it_with, "-noprelude")
     THEN(compilation_succeeded)
     WHEN(i_run_it)
